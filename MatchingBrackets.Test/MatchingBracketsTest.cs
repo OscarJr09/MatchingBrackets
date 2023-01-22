967047b0ef85b9ed81f9cdb3feb9bc55e4adba13
Oscar Domingo <oscar.domingo@axes.ai>
2023-01-22 18:57:48 +0800

    Add project files.

---
+++ MatchingBrackets.Test/MatchingBracketsTest.cs
@@ -0,0 +1,17 @@
+﻿namespace MatchingBrackets.Test
+{
+    public class MatchingBracketsTest
+    {
+        [Fact]
+        public void IsMatchingBracketsTest() { 
+            var matchingBrackets = new MatchingBrackets();
+
+            Assert.True(matchingBrackets.IsMatchingBrackets("{}"));
+            Assert.False(matchingBrackets.IsMatchingBrackets("}{"));
+            Assert.False(matchingBrackets.IsMatchingBrackets("{{}"));
+            Assert.False(matchingBrackets.IsMatchingBrackets("{}}"));
+            Assert.True(matchingBrackets.IsMatchingBrackets(""));
+            Assert.True(matchingBrackets.IsMatchingBrackets("{abc...xyz}"));
+        }
+    }
+}
---
+++ MatchingBrackets/MatchingBrackets.cs
@@ -0,0 +1,32 @@
+﻿namespace MatchingBrackets
+{
+    public class MatchingBrackets
+    {
+        public MatchingBrackets() { }
+
+        public bool IsMatchingBrackets(string str)
+        {
+            if (string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str))
+                return true;
+
+            const char openBrace = '{';
+            const char closeBrace = '}';
+
+            int openBracesCount = str.Count(s => s == openBrace);
+            int closeBracesCount = str.Count(s => s == closeBrace);
+
+            if (openBracesCount != closeBracesCount)
+                return false;
+
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == openBrace) count++;
+                if (str[i] == closeBrace) count--;
+                if (count < 0) return false;
+            }
+
+            return count == 0;
+        }
+    }
+}
